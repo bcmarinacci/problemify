aba9907686657a390e253d1d1b2845b6142263e1
Benji Marinacci <bcmarinacci@icloud.com>
2016-04-23 19:30:06 -0700

    Implement CLI

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,17 +1,47 @@
 #!/usr/bin/env node
 
 import meow from 'meow';
+import {ncp} from 'ncp';
+import pify from 'pify';
+import prepareProblem from '../lib/prepareProblem';
+import prepareSolution from '../lib/prepareSolution';
 
 const cli = meow(`
   Usage
-    $ problemify <input-directory>
-
-  Options
-    --problem Output problem repository
-    --solution Output solution repository
+    $ problemify <directory>
 
   Example
-     $ problemify death-star-plans
-`);
+    $ problemify death-star-plans
+`, {
+  string: ['_']
+});
 
-console.log(cli);
+const problemify = async function (srcDir, destDir, cb) {
+  // TODO: format `srcDir`
+  // TODO: log relative paths of files written
+  const ncpOptions = {
+    stopOnErr: true,
+    transform(srcFileStream, destFileStream) {
+      let file = '';
+      srcFileStream.on('data', chunk => {
+        file += chunk;
+      });
+
+      srcFileStream.on('end', async () => {
+        await pify(destFileStream.write(cb(file)));
+        console.log('file written');
+      });
+    }
+  };
+
+  await pify(ncp)(srcDir, destDir, ncpOptions);
+};
+
+const sourceDirectory = cli.input[0];
+console.log('input: ', sourceDirectory);
+
+const problemDestination = `${sourceDirectory}-problem`;
+const solutionDestination = `${sourceDirectory}-solution`;
+
+problemify(sourceDirectory, problemDestination, prepareProblem);
+problemify(sourceDirectory, solutionDestination, prepareSolution);
---
+++ test/bin/cli.test.js
@@ -0,0 +1,130 @@
+import {readFile} from 'fs';
+import {execFile} from 'child_process';
+import pify from 'pify';
+import test from 'ava';
+
+const fixturePaths = {
+  problemForEach: '../fixtures/mockUtils-problem/higherOrder/forEach/forEach.js',
+  problemMap: '../fixtures/mockUtils-problem/higherOrder/map/map.js',
+  problemPrint: '../fixtures/mockUtils-problem/print/print.js',
+  solutionForEach: '../fixtures/mockUtils-solution/higherOrder/forEach/forEach.js',
+  solutionMap: '../fixtures/mockUtils-solution/higherOrder/map/map.js',
+  solutionPrint: '../fixtures/mockUtils-solution/print/print.js'
+};
+
+const results = {};
+results.problemForEach = `// Write a function that invokes a callback on each element in an array
+// problem code
+const forEach = function () {
+  // your code here
+
+};
+
+// shared code
+const droids = ['R2-D2', 'C-3PO', 'BB-8'];
+
+forEach(droids, console.log);
+// R2-D2
+// C-3PO
+// BB-8
+`;
+
+results.problemMap = `// Write a function that returns an array of mapped elements
+// problem code
+const map = function () {
+  // your code here
+
+};
+
+// shared code
+const numbers = [1, 2, 3, 4, 5];
+
+map(numbers, n => n ** 2);
+// [1, 4, 9, 16, 25]
+`;
+
+results.problemPrint = `// Write a function that prints a value to the console
+// problem code
+const print = function () {
+  // your code here
+
+};
+
+// shared code
+const jedi = 'Qui-Gon';
+
+print(jedi);
+// Qui-Gon
+`;
+
+results.solutionForEach = `// Write a function that invokes a callback on each element in an array
+// solution code
+const forEach = function (array, cb) {
+  for (let i = 0; i < array.length; i++) {
+    cb(array[i], i, array);
+  }
+};
+
+// shared code
+const droids = ['R2-D2', 'C-3PO', 'BB-8'];
+
+forEach(droids, console.log);
+// R2-D2
+// C-3PO
+// BB-8
+`;
+
+results.solutionMap = `// Write a function that returns an array of mapped elements
+// solution code
+const map = function (array, cb) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(cb(array[i], i, array));
+  }
+
+  return result;
+};
+
+// shared code
+const numbers = [1, 2, 3, 4, 5];
+
+map(numbers, n => n ** 2);
+// [1, 4, 9, 16, 25]
+`;
+
+results.solutionPrint = `// Write a function that prints a value to the console
+// solution code
+const print = function (value) {
+  console.log(value);
+};
+
+// shared code
+const jedi = 'Qui-Gon';
+
+print(jedi);
+// Qui-Gon
+`;
+
+test('output', async t => {
+  try {
+    await pify(execFile)('../../dist/bin/cli.js', ['../fixtures/mockUtils']);
+    const pathKeys = Object.keys(fixturePaths);
+
+    // Store output file contents in an array
+    const files = await Promise.all(pathKeys.map(pathKey => {
+      const path = fixturePaths[pathKey];
+
+      return pify(readFile)(path, 'utf8');
+    }));
+
+    // Assert whether the contents of each output file is equal to each desired result
+    files.forEach((file, i) => {
+      const pathKey = pathKeys[i];
+      const result = results[pathKey];
+
+      t.is(file, result);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
