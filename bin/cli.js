9b780809d32b9d019ed8831c234f6b2f0cca2fe1
Benji Marinacci <bcmarinacci@icloud.com>
2016-05-21 21:58:18 -0700

    Move main function into separate file

--- bin/cli.js
+++ bin/cli.js
@@ -1,72 +1,27 @@
 #!/usr/bin/env node
 'use strict';
 
+const args = require('args');
 const chalk = require('chalk');
-const meow = require('meow');
-const ncp = require('ncp');
-const pify = require('pify');
-const rejectGitignore = require('../lib/rejectGitignore');
 const formatPath = require('../lib/formatPath');
 const prepareProblem = require('../lib/prepareProblem');
 const prepareSolution = require('../lib/prepareSolution');
+const copyDirectory = require('../lib/copy-directory');
 
-const problemify = function (srcDir, destDir, cb) {
-  // match root directory name without slashes
-  const regex = /.*?\/*([^<>:"\/\\|?*]+)\/*$/;
-  const rootDirname = srcDir.replace(regex, '$1');
-  let fileCount = 0;
-  const ncpOptions = {
-    stopOnErr: true,
-    filter: rejectGitignore,
-    transform(fileReadable, fileWriteable) {
-      let file = '';
-      fileReadable.on('data', chunk => {
-        file += chunk;
-      });
+args
+  .command('<directory>', 'The directory to copy')
+  .parse(process.argv);
 
-      fileReadable.on('end', () => {
-        const srcName = fileReadable.path;
-        const srcIndex = srcName.lastIndexOf(rootDirname);
-        const destName = fileWriteable.path;
-        const destIndex = destName.lastIndexOf(rootDirname);
-        console.log(`${++fileCount}. ${srcName.slice(srcIndex)} → ${destName.slice(destIndex)}`);
-        fileWriteable.end(cb(file));
-      });
-
-      fileReadable.on('error', err => {
-        throw err;
-      });
-
-      fileWriteable.on('error', err => {
-        throw err;
-      });
-    }
-  };
-
-  return pify(ncp)(srcDir, destDir, ncpOptions);
-};
-
-const cli = meow(`
-  Usage
-    $ problemify <repository>
-
-  Example
-    $ problemify kessel-run
-    $ ls
-    kessel-run    kessel-run-problem    kessel-run-solution
-`);
-
-const cliInput = cli.input[0];
-if (!cliInput) {
-  console.log(cli.help);
-  process.exit(1);
+const argv = args.sub;
+if (argv.length === 0) {
+  args.showHelp();
 }
 
-const formattedCliInput = formatPath(cliInput);
-const problemDest = `${formattedCliInput}-problem`;
-const solutionDest = `${formattedCliInput}-solution`;
-problemify(formattedCliInput, problemDest, prepareProblem)
-  .then(() => problemify(formattedCliInput, solutionDest, prepareSolution))
+const formattedInput = formatPath(argv[0]);
+const problemDest = `${formattedInput}-problem`;
+const solutionDest = `${formattedInput}-solution`;
+copyDirectory(formattedInput, problemDest, prepareProblem)
+  .then(() => copyDirectory(formattedInput, solutionDest, prepareSolution))
   .catch(err => {
     console.log(chalk.yellow("Something went wrong. See 'problemify --help' for usage information."));
     console.error(chalk.red(err.stack || err));
