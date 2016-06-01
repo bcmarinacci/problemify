58d562952e9b167d984cda508a1df3f2546e7a75
Benji Marinacci <bcmarinacci@icloud.com>
2016-06-01 01:43:05 -0700

    Remove comments

--- bin/cli.js
+++ bin/cli.js
@@ -4,9 +4,6 @@
 const args = require('args');
 const chalk = require('chalk');
 const formatPath = require('../lib/format-path');
-// const prepareProblem = require('../lib/prepare-problem');
-// const prepareSolution = require('../lib/prepare-solution');
-// const copyDirectory = require('../lib/copy-directory');
 const problemify = require('../lib/problemify');
 
 args
@@ -23,13 +20,3 @@ problemify(formattedInput).catch(err => {
   console.error(chalk.red(err.stack || err));
   process.exit(1);
 });
-
-// const problemDest = `${formattedInput}-problem`;
-// const solutionDest = `${formattedInput}-solution`;
-// copyDirectory(formattedInput, problemDest, prepareProblem)
-//   .then(() => copyDirectory(formattedInput, solutionDest, prepareSolution))
-//   .catch(err => {
-//     console.log(chalk.yellow("Something went wrong. See 'problemify --help' for usage information."));
-//     console.error(chalk.red(err.stack || err));
-//     process.exit(1);
-//   });
