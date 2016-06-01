76092eec942b65c2a94cd9e6535ddb5babe3f96c
Benji Marinacci <bcmarinacci@icloud.com>
2016-06-01 16:33:55 -0700

    Tidy up dependencies

--- bin/cli.js
+++ bin/cli.js
@@ -3,19 +3,16 @@
 
 const args = require('args');
 const chalk = require('chalk');
-const formatPath = require('../lib/format-path');
 const problemify = require('../lib/problemify');
 
-args
-  .parse(process.argv, { value: '<directory>' });
+args.parse(process.argv, { value: '<directory>' });
 
 const argv = args.sub;
 if (argv.length === 0) {
   args.showHelp();
 }
 
-const formattedInput = formatPath(argv[0]);
-problemify(formattedInput).catch(err => {
+problemify(argv[0]).catch(err => {
   console.log(chalk.yellow("Something went wrong. See 'problemify --help' for usage information."));
   console.error(chalk.red(err.stack || err));
   process.exit(1);
